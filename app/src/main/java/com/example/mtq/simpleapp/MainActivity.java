11bb5fcecfce704ea07c835ca84072e27da1232c
mtq1992 <1219746766@qq.com>
2016-04-16 20:11:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mtq/simpleapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mtq.simpleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+import org.jdom2.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Document doc = new Document();
+
+        Gson g = new Gson();
+    }
+}
